b2a9c1751b3c3ea1932bea3ba975e1232769fdb9
Luana Thomas <luana.thomas@hotmail.com>
2023-03-06 18:17:28 -0300

    Questão 3

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
         Carro utilitario = new Carro("Utilitário", TipoCombustivel.DIESEL, 5, 70);
+        Carro suv = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
 
 
         System.out.println("Tipos de veiculos:");
